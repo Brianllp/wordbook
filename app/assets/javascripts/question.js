e65636d2eafbe03ccffe3f4dfa29a92a016791ef
Brianllp <44490577+Brianllp@users.noreply.github.com>
2020-02-06 08:39:40 +0900

    取得したデータを表示

---
+++ app/assets/javascripts/question.js
@@ -0,0 +1,22 @@
+
+// $(document).ready(function() {
+//   $("div").text("onomatopoeia");
+// });
+
+// document.addEventListener('turbolinks:load', function() {
+//   document.querySelectorAll('div').forEach(function(div) {
+//     div.addEventListener('mouseover', function(e) {
+//       e.currentTarget.style.backgroundColor = '#eff';
+//     });
+
+//     div.addEventListener('mouseover', function(e) {
+//       e.currentTarget.style.backgroundColor = '';
+//     });
+//     console.log("aaaa");
+//   });
+// });
+
+// document.getElementById("test").onclick = function() {
+//   const random = Math.floor( Math.random() * 50 );
+//   console.log(random);
+// };
--- app/controllers/question_controller.rb
+++ app/controllers/question_controller.rb
@@ -1,4 +1,8 @@
 class QuestionController < ApplicationController
   def main
+    @questions = Question.all.order("created_at ASC")
+    # @questions = Question.all(params[:question])
+    # @questions = Question.find(random.rand(50))
+    # @questions = Question.offset( rand(Question.count) ).first
   end
 end
