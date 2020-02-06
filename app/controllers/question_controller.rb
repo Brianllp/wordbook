7150cb15848ba3047b203ea137f9c98b6e947b2b
Brianllp <44490577+Brianllp@users.noreply.github.com>
2020-02-06 15:27:06 +0900

    単語帳ページの作成

--- app/controllers/question_controller.rb
+++ app/controllers/question_controller.rb
@@ -1,8 +1,29 @@
 class QuestionController < ApplicationController
+  require 'byebug'
+
   def main
+    # debugger
     @questions = Question.all.order("created_at ASC")
+    # logger.debug @questions.to_yaml
+
     # @questions = Question.all(params[:question])
     # @questions = Question.find(random.rand(50))
     # @questions = Question.offset( rand(Question.count) ).first
+  end
+
+  def result
+    debugger
+    @result = Question.find_by(answer: result_params[:answer])
+    # @result = Question.find_by(answer: params[:answer])
+    debugger
+    if @result
+      redirect_to question_result_path, notice: "回答しました"
+    end
+  end
+
+  private
+
+  def result_params
+    params.require(:question).permit(:answer)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'menu#index'
   get 'question/main'
+  post 'question/main' => 'question#result'
+  get 'question/result'
+  # post 'question/result'
 end
